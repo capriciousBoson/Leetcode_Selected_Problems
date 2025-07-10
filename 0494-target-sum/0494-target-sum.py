b33b5721498659ee826e4969796a7ab645f0e68f
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-07-10 17:18:59 -0500

    Time: 239 ms (10.36%), Space: 38.6 MB (17.83%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        n = len(nums)
+
+        memo = {}
+        def dfs(i,x):
+            if i>=n:
+                print(f"final path sum : {x}")
+                if x==target:
+                    return 1
+                else:
+                    return 0
+            if (i,x) not in memo:
+                left = dfs(i+1, x+nums[i])
+                right = dfs(i+1, x-nums[i])
+                memo[(i,x)] = left + right
+
+            return memo[(i,x)]
+
+
+        res = dfs(0,0)
+        return res
+        
