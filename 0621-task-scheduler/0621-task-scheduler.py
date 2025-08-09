e6c9bf0fec0f61690c4d5d8d5a542b1902f9832c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-08 20:57:33 -0500

    Time: 23 ms (80.72%), Space: 19.2 MB (65.24%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -3,14 +3,14 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         c = Counter(tasks)
         freq = [item[1] for item in sorted(c.items(), key=lambda x: x[1])]
-        print(freq)
+        # print(freq)
         f1 = freq.pop()
         extra_places = (f1 - 1)*n
 
-        print(f"f1 = {f1}, extraplaces = {extra_places}")
+        # print(f"f1 = {f1}, extraplaces = {extra_places}")
         while freq:
             f = freq.pop()
-            print(f"current frequency = {f}, occ = { min(f1-1, f)} ")
+            # print(f"current frequency = {f}, occ = { min(f1-1, f)} ")
 
             extra_places -= min(f1-1, f)
         return len(tasks) + max(0, extra_places)
