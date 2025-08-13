a251ec3ecf167b3339b870480d4e4932241adeeb
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-13 11:17:26 -0500

    Time: 7613 ms (5%), Space: 26 MB (5.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,43 +4,50 @@ class Solution:
     #         if a<=i<=b : return jumps
 
     def jump(self, nums: List[int]) -> int:
-        # n = len(nums)
-        # if n==1:
-        #     return 0
-
-        # max_reach = 0 
-        # ranges = [[0,0,0]]
+        n = len(nums)
+        if n<=1:
+            return 0
+        
+        # dp = [[float('inf') for _  in range(n+1)] for __ in range(n+1)]
+        # dp[0][0] = 0 
 
         # for i in range(n):
-        #     if i + nums[i] > max_reach:
-        #         current_jumps = 0
-        #         for a,b,j in ranges:
-        #             if a<=i<=b:
-        #                 current_jumps = j
-        #                 break
-        #         current_jumps += 1
-        #         max_reach = i+nums[i]
-        #         ranges.append([i, max_reach, current_jumps])
+        #     min_jums = float('inf')
+        #     for j in range(1, nums[i]+1):
+        #         min_jumps = min(min_jumps, dp[i+x][j+1])
 
-        #         if max_reach >= n-1:
-        #             return current_jumps
-        n = len(nums)
-        if n <= 1:
-            return 0
 
-        jumps = 0
-        end = 0        # end of the current jump's range
-        farthest = 0   # farthest we can reach while scanning this range
+        memo = {}
+        def dfs(i):
+            if i >=n-1:
 
-        # We stop at n-2 because once we reach the last index, no more jumps are needed
-        for i in range(n - 1):
-            farthest = max(farthest, i + nums[i])
-            if i == end:
-                jumps += 1
-                end = farthest
-                if end >= n - 1:
-                    break
-        return jumps
+                return 0
+            
+            if (i) not in memo:
+                min_jumps = float('inf')
+                for x in range(1,nums[i]+1):
+                    min_jumps = min(min_jumps, 1+dfs(i+x))
+                memo[i] = min_jumps
+            return memo[i]
+
+        return dfs(0)
+
+        
+
+
+        # jumps = 0
+        # end = 0        # end of the current jump's range
+        # farthest = 0   # farthest we can reach while scanning this range
+
+        # # We stop at n-2 because once we reach the last index, no more jumps are needed
+        # for i in range(n - 1):
+        #     farthest = max(farthest, i + nums[i])
+        #     if i == end:
+        #         jumps += 1
+        #         end = farthest
+        #         if end >= n - 1:
+        #             break
+        # return jumps
 
 
 
