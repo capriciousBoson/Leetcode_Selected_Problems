e2830d228a00bd8faf6c2c01e834965b7932914b
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-15 16:40:23 -0500

    Time: 19 ms (5.25%), Space: 20.9 MB (62.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,17 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x: x[0])
-        n = len(intervals)
-        res = [intervals[0]]
+        # intervals.sort(key=lambda x: x[0])
+        # n = len(intervals)
+        heap = []
+        n = 0
+        for interval in intervals:
+            n+=1
+            heapq.heappush(heap, interval)
+
+        res = [heapq.heappop(heap)]
         i = 1
         while i < n:
-            
-            if intervals[i][0] <= res[-1][1]:
-                end = max(intervals[i][1], res[-1][1])
+            current = heapq.heappop(heap)
+            if current[0] <= res[-1][1]:
+                end = max(current[1], res[-1][1])
                 res[-1][1] = end
                 i += 1
             else:
-                res.append(intervals[i])
+                res.append(current)
                 i += 1
         return res
                 
