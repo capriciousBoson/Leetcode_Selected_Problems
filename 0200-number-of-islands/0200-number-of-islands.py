7d1f2712fc7d503be35c757c70d05dabd9d71888
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-24 12:37:01 -0500

    Time: 242 ms (66.13%), Space: 20.2 MB (57.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,28 +2,28 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        visited = [[False for _ in range(n)] for __ in range(m)]
+        # visited = [[False for _ in range(n)] for __ in range(m)]
         res  = 0
         
         dirs = [(0,1), (1,0), (0,-1), (-1,0)]
         for i in range(m):
             for j in range(n):
                 # print(f"\ngrid[{i}][{j}] : {grid[i][j]} | visited : {visited[i][j]}")
-                if grid[i][j]=="1" and visited[i][j]==False:
+                if grid[i][j]=="1":
                     res += 1
                     # print(f" updated res : {res}")
 
                     Q = collections.deque()
                     Q.append((i,j))
-                    visited[i][j] = True
+                    grid[i][j] = "#"
 
                     while Q:
                         i_, j_ = Q.popleft()
                         for dx, dy in dirs:
                             x,y = i_ + dx, j_ + dy
-                            if 0<=x<m and 0<=y<n and grid[x][y]=="1" and not visited[x][y]:
+                            if 0<=x<m and 0<=y<n and grid[x][y]=="1" :
                                 Q.append((x,y))
-                                visited[x][y] = True
+                                grid[x][y] = "#"
 
                 
         return res
