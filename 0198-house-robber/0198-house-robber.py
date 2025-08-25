937f83976edb551780cef9d42356d317c45b74b0
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-25 15:58:05 -0500

    Time: 0 ms (100%), Space: 17.9 MB (33.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -10,9 +10,12 @@ class Solution:
         #     return memo[i]
         # return robber(0)
         
-        dp = [0 for _  in range(n+1)]
-        dp[n-1] = nums[n-1]
+        # dp = [0 for _  in range(n+1)]
+        dp_i1 = nums[n-1]
+        dp_i2 = 0
 
         for i in range(n-2, -1, -1):
-            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
-        return dp[0]
+            x = max(nums[i] + dp_i2, dp_i1)
+            dp_i2 = dp_i1
+            dp_i1 = x
+        return max(dp_i1, dp_i2)
