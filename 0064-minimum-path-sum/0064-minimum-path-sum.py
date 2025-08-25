bc8e525e38054f702649cc1f39518bc817d8cd73
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-25 18:14:01 -0500

    Time: 23 ms (30.36%), Space: 20.3 MB (39.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -28,13 +28,15 @@ class Solution:
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
                 if i==m-1 and j==n-1 : continue
-                down = float('inf')
-                right = float('inf')
+                # down = float('inf')
+                # right = float('inf')
 
-                if i <m-1:
-                    down = dp[i+1][j]
-                if j < n-1:
-                    right = dp[i][j+1]
+                # if i <m-1:
+                #     down = dp[i+1][j]
+                # if j < n-1:
+                #     right = dp[i][j+1]
+                down = dp[i+1][j]
+                right = dp[i][j+1]
                 
                 dp[i][j] = grid[i][j] + min(down, right)
         return dp[0][0]
