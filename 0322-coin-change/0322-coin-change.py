77893ac8b4a39f5e207f06d30fe8300f23c4dbe6
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-26 10:29:19 -0500

    Time: 1149 ms (17.72%), Space: 45.9 MB (8.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,42 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if not coins: return 0
+        if amount==0: return 0
+        memo = {}
+        def dfs(x):
+            if x==0:
+                return 0
+            if x<0 : return float('inf')
 
+            if x not in memo:
+                res = float('inf')
+                for c in coins:
+                    res = min(res, 1+dfs(x-c))
+                memo[x] = res
+            return memo[x]
+        n = dfs(amount)
+        return n if n!=float('inf') else -1
 
-        dp = [float('inf') for _ in range(amount + 1)]
-        dp[0] = 0
-        
-        for i in range( amount+1):
-            for coin in coins:
-                if i+coin <= amount:
-                    # print(f"for amount(i) : {i}, coin : {coin} dp[i+coin] : {dp[i+coin]}")
-                    dp[i+coin] = min(dp[i+coin], 1+dp[i])
-                    # print(f"updated  dp[i+coin] : {dp[i+coin]}")
-
-        # print(f"final dp array : {dp}")
-        return dp[amount] if dp[amount] != float('inf') else -1
-
-        # memo = {}
-
-        # def dfs( money):
-        
-        #     if money == amount:
-        #         return 0
-        #     if money > amount:
-        #         return float('inf')
-
-        #     if money in memo: 
-        #         return memo[money]
-        #     res = float('inf')
-        #     for coin in coins:
-        #         res =min(res, 1+ dfs(money+coin))
-
-        #     memo[money] =res
-
-        #     return  memo[money]
-        
-        # n = dfs(0)
-        # if n==float('inf'): return -1
-        # return n
-
-            
         
