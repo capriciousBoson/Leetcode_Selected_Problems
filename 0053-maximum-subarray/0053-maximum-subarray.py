2addb0d4b9998446a7bb5c2e2fa7e47ad5dd515f
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-08-27 21:34:28 -0500

    Time: 117 ms (5.06%), Space: 33.1 MB (6.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,11 +4,17 @@ class Solution:
 
         res = nums[0]
         current_max = 0
+        start = 0
+        end = 0
         for i in range( len(nums)):
             current_max += nums[i]
+            end = i
             res = max(res, current_max)
             if current_max <=0:
                 current_max = 0
+                start = i+1
+                end = i+1
+        print(nums[start:end+1])
         return res
 
             
