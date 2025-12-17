f4e7a82e388bdcd73f263317a1d249676bd71e91
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2025-12-17 15:58:45 -0600

    Time: 3 ms (66.18%), Space: 17.8 MB (86.17%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -14,33 +14,33 @@ class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         left, right = max(nums), sum(nums)
 
-        res = float('inf')
+        # res = float('inf')
 
         while left <= right:
 
             mid = (left + right)//2
             partitions = self.countPartitions(nums, mid)
-            print(f"left : {left}, right : {right}, mid: {mid} | partitions : {partitions}")
+            # print(f"left : {left}, right : {right}, mid: {mid} | partitions : {partitions}")
             if partitions == k:
                 right = mid-1
-                res = min(res, mid)
+                # # res = min(res, mid)
             elif partitions <k:
-                res = min(res, mid)
+                # # res = min(res, mid)
                 right = mid-1
             else:
                 left = mid+1
         
-        print(f"max subarray sum limit : {res} ")
+        # print(f"max subarray sum limit : {res} ")
 
-        ans = -float('inf')
-        s = 0
-        for n in nums:
-            if s+n <= res:
-                s += n
-            else:
-                s = n
-            ans = max(ans, s)
-        print(f"max subassray sum : {ans}")
-        return ans
+        # ans = -float('inf')
+        # s = 0
+        # for n in nums:
+        #     if s+n <= left:
+        #         s += n
+        #     else:
+        #         s = n
+        #     ans = max(ans, s)
+        # # print(f"max subassray sum : {ans}")
+        return left
 
         
