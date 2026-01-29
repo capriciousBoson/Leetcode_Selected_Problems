2ca82e8dab4718538bfc82632c0779f636d79d23
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-01-29 12:02:13 -0600

    Time: 3 ms (10.59%), Space: 19.3 MB (36.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,18 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
         n = len(nums)
-        if n<2: return max(nums)
-        
-        def rob_(i):
+
+        # edge case:
+        if n==1: return nums[0]
+        memo = {}
+
+        def rob(i):
             if i>=n:
                 return 0
             if i not in memo:
-                memo[i] = max(nums[i] + rob_(i+2), rob_(i+1))
-            return memo[i]
+                memo[i] = max(nums[i]+rob(i+2), rob(i+1))
+            
+            # print(f"memo[{i}] : {memo[i]}")
+            return  memo[i]
+        
+        # skip house 0
+        a = rob(1)
+        # print(f"a : {a}")
 
-        x = rob_(1)
-        n -=1 
+        # rob house zero, skip last one
         memo = {}
-        y = rob_(0)
-        return max(x,y)
+        n = n-1
+        b = rob(0)
+        # print(f"b : {b}")
+
+        return max(a,b)
