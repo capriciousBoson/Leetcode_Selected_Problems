76926bd8f6a80e505544832b7c50461b7d3d980e
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-01-29 14:02:59 -0600

    Time: 852 ms (44.86%), Space: 160.2 MB (13.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,22 +3,69 @@ class Solution:
         total = sum(nums)
         if total %2 !=0 or len(nums) < 2:
             return False
-
+        
+        n = len(nums)
+        target = total //2
         memo = {}
-        def fun(i,target):
-            if target==0:
+
+        def partitionSum(i,x):
+            if x==target:
                 return True
-            if i==0:
-                return target==nums[0]
+            if i>=n:
+                return False
+            
+            if (i,x) not in memo:
+                take  = False
+                if x + nums[i]==target:
+                    take = True
+                if x + nums[i] < target:
+                    take = partitionSum(i+1, x+nums[i])
+                if take: return True
+                
+                not_take = partitionSum(i+1, x)
+                if not_take: return True
 
-            if (i, target) not in memo:
-                take = False
-                if target >= nums[i]:
-                    take =  fun(i-1, target - nums[i])
-                if take==True: return True
-                not_take = fun(i-1, target)
-                if not_take==True: return True
+                memo[(i,x)] = take or not_take
+ 
+            return memo[(i,x)]
 
-                memo[(i,target)] = take or not_take
-            return memo[(i, target)]
-        return fun(len(nums)-1, total//2)
+
+        return partitionSum(0,0)
+            
+
+        # dp = [[False for _ in range((total//2) + 1)] for __ in range(n)]
+        # if nums[0] <=total//2:
+        #     dp[0][nums[0]] = True
+        # for i in range(n):
+        #     dp[i][0] = True
+        
+        # for i in range(1,n):
+        #     for target in range(1,(total//2)+1):
+        #         take = False
+        #         if nums[i] <= target:
+        #             take = dp[i-1][target-nums[i]]
+        #         not_take = dp[i-1][target]
+        #         dp[i][target] = take or not_take
+        # return dp[n-1][total//2]
+
+        
+
+
+        # memo = {}
+        # def fun(i,target):
+        #     if target==0:
+        #         return True
+        #     if i==0:
+        #         return target==nums[0]
+
+        #     if (i, target) not in memo:
+        #         take = False
+        #         if target >= nums[i]:
+        #             take =  fun(i-1, target - nums[i])
+        #         if take==True: return True
+        #         not_take = fun(i-1, target)
+        #         if not_take==True: return True
+
+        #         memo[(i,target)] = take or not_take
+        #     return memo[(i, target)]
+        # return fun(len(nums)-1, total//2)
