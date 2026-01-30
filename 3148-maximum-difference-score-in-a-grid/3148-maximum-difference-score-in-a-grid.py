59ce35c975bc5e64c58155498eedb83393e443af
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-01-30 00:52:25 -0600

    Time: 172 ms (91.6%), Space: 33.2 MB (33.61%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.py
@@ -2,36 +2,31 @@ class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        @lru_cache(None)
-        def dfs(i,j):
-            if i >= m or j >= n:
-                return 0 
+        # pad the grid with a top row  and left column consisting of float('inf')
+        #  pad left
+        grid = [[float('inf')] + row for row in grid]
 
-            max_val = float("-inf")
+        # pad top
+        grid = [[float('inf') for _ in range(n+1)]] + grid
+        # print(grid)
+        res = -float('inf')
+        # memo = {}
 
-            if i+1 < m:
-                max_val = max(max_val,grid[i+1][j]-grid[i][j]+dfs(i+1,j))
+        for i in range(1,m+1):
+            for j in range(1, n+1):
 
-            if j+1 < n:
-                max_val = max(max_val,grid[i][j+1]-grid[i][j]+dfs(i,j+1))
+                x = min(grid[i-1][j], grid[i][j-1])
+                # print(f"previous minimum at {i,j} = {x}")
+                res = max(res, grid[i][j]-x)
 
-            return max(0,max_val)
+                grid[i][j] = min(x, grid[i][j])
+                # print(f"grid[{i}][{j}] = {grid[i][j]}")
 
-        mx_val, mx_val1 = float("-inf"), float("-inf")
-
-        for i in range(m):
-            for j in range(n):
-                mx_val = max(mx_val,dfs(i,j))
-                if (i+1 < m):
-                    mx_val1 = max(mx_val1,grid[i+1][j]-grid[i][j])
-                if (j+1 < n):
-                    mx_val1 = max(mx_val1,grid[i][j+1]-grid[i][j])
-
-        if mx_val == 0:
-            return mx_val1
-
-        return mx_val 
+        for r in grid:
+            print(r)
+        return res
 
 
 
+            
         
