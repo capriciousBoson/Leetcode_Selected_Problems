c316705392314e82a527c90e566928c8d67ff3cf
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-01-30 23:52:00 -0600

    Time: 951 ms (23.22%), Space: 71.1 MB (7.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,16 +3,36 @@ class Solution:
         if not coins: return 0
         if amount==0: return 0
 
-        n = len(coins)
-        dp = [float('inf') for _ in range(amount+1)]
-        dp[0] = 0
+        memo = {}
 
-        for a in range(1, amount+1):
-            for coin in coins:
-                if a-coin >=0:
-                    dp[a] = min(dp[a], 1+dp[a-coin])
+        def fun(x):
+            if x== 0:
+                return 0
+            elif x < 0:
+                return float('inf')
+            
+            if x not in memo :
+                res = float('inf')
+
+                for c in coins:
+                    res = min(res, 1 + fun(x-c))
+                memo[x] = res
+
+            return memo[x]
+        n = fun(amount)
+        return n if n!=float('inf') else -1
+
+
+        # n = len(coins)
+        # dp = [float('inf') for _ in range(amount+1)]
+        # dp[0] = 0
+
+        # for a in range(1, amount+1):
+        #     for coin in coins:
+        #         if a-coin >=0:
+        #             dp[a] = min(dp[a], 1+dp[a-coin])
                     
-        return dp[amount] if dp[amount]!=float('inf') else -1
+        # return dp[amount] if dp[amount]!=float('inf') else -1
 
         # memo = {}
         # def dfs(x):
