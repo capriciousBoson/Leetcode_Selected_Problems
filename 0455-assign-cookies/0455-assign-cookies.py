30f9c4af7751f54b354d4410d420a3e4df9c35ed
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-01-30 23:36:52 -0600

    Time: 23 ms (57.08%), Space: 21.6 MB (39.92%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,7 +1,7 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort(reverse=True) #g = 6 5 3 2, 1
-        s.sort(reverse=True) #s = 5 4 1
+        g.sort() #g = 6 5 3 2, 1
+        s.sort() #s = 5 4 1
         res = 0                 # res =  2
         while g and s:
             if s[-1]>=g[-1]:
@@ -9,7 +9,7 @@ class Solution:
                 g.pop()
                 s.pop()
             else:
-                s.pop()
+                g.pop()
         return res
 
 # greed = [3,7,9] , cookie = [10,9,6]
