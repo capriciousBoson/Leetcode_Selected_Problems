5f38b4a4d323c42c67b2465f146479642038490c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-08 18:55:25 -0600

    Time: 4254 ms (29.27%), Space: 364.8 MB (16.26%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -6,26 +6,26 @@ class Solution:
 
         prefix = [[sum(pile[0:i+1]) for i in range(len(pile))] for pile in piles]
         print(prefix)
-        # memo = {}
-        @lru_cache(None)
+        memo = {}
+        # @lru_cache(None)
         def dfs(count, pile_idx):
             if count <0  or pile_idx>=n:
                 return 0
 
             # print(f"\nnums to take : {count} | currently at piles[{pile_idx}] : {piles[pile_idx]}")
             
-            # if (count, pile_idx) not in memo:
-            s = dfs(count, pile_idx+ 1)
-            limit = min(count, len(piles[pile_idx]))
-            for i in range(1,limit+1):
-                # if i <= len(prefix[pile_idx]):
-                    
-                    # print(f"taking {i} for current pile with sum : {sum(piles[pile_idx][0:i])}")
-                s = max(s, prefix[pile_idx][i-1] + dfs(count-i, pile_idx+1) )
-                    # print(f"finally got s = {s} for pile : {piles[pile_idx]} for first : {i} of :{count-i}")
-            # memo[(count, pile_idx)] = s
-            # return memo[(count, pile_idx)]
-            return s
+            if (count, pile_idx) not in memo:
+                s = dfs(count, pile_idx+ 1)
+                limit = min(count, len(piles[pile_idx]))
+                for i in range(1,limit+1):
+                    # if i <= len(prefix[pile_idx]):
+                        
+                        # print(f"taking {i} for current pile with sum : {sum(piles[pile_idx][0:i])}")
+                    s = max(s, prefix[pile_idx][i-1] + dfs(count-i, pile_idx+1) )
+                        # print(f"finally got s = {s} for pile : {piles[pile_idx]} for first : {i} of :{count-i}")
+                memo[(count, pile_idx)] = s
+            return memo[(count, pile_idx)]
+            # return s
         
         return dfs(k, 0)
 
