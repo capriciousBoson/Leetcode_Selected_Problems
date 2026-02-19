1393d396123ebaaf28cf1496a077b6b16d03b502
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-19 12:19:44 -0600

    Time: 21 ms (23.53%), Space: 19.5 MB (6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,20 +1,29 @@
-from collections import deque
+from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        res = 0
-        found = {}
-        l, r = 0,0
-
-        while r < len(s):  
-            if s[r] in found and found[s[r]] >= l:
-                l = found[s[r]]+1
-
-            found[s[r]] = r
-            length = r-l +1
-            res = max(length, res)
+        if len(s)==0: return 0
+        l,r = 0,0
+        dq = defaultdict(int)
+        dq[s[l]] = 1
+        res = 1
+        while r < len(s)-1:
+            # print(f"\nstarting with string : {s[l:r+1]}  \tres : {res}")
+            
             r += 1
+            dq[s[r]] += 1
+            # print(f"added one character : {s[l:r+1]}")
+            
+            # print(f"dq : {dq}")
+            while dq[s[r]] > 1:
+                dq[s[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+            # print(f"final string : {s[l:r+1]} \t\t& res = {res}")
         return res
+
+
+
 
 
 
