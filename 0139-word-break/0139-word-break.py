1c14d10d57dee9e08cca2664e910627d3ff00fdf
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-19 19:16:01 -0600

    Time: 4 ms (39.38%), Space: 20.7 MB (5.23%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,24 +1,50 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def insert(self, word):
+        current = self.root
+
+        for c in word:
+            if c not in current.children:
+                current.children[c] = Node()
+            current = current.children[c]
+        current.isEndOfWord = True
+    
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        words = set(wordDict)
-        memo = {}
-        def dfs(i,j):
-            if i >= n:
-                return True
-            if j >= n:
-                return False
+        # words = set(wordDict)
+        trie = Trie()
+        for word in wordDict:
+            trie.insert(word)
 
-            if (i,j) not in memo:
-                if s[i:j+1] in words:
-                    take = dfs(j+1, j+1)
-                    if take:
-                        return True
-                    not_take = dfs(i, j+1)
-                    memo[(i,j)] = take or not_take
-                    
-                else:
-                    memo[(i,j)] =  dfs(i, j+1)
-            return memo[(i,j)]
-        return dfs(0,0)
+        memo = {}
+        def dfs(idx):
+            if idx >= n:
+                return True
+            if idx not in memo:
+                memo[idx] = False
+                current = trie.root
+
+                for i in range(idx, n):
+                    if s[i] in current.children:
+                        current = current.children[s[i]]
+                        if current.isEndOfWord:
+                            memo[idx] = memo[idx] or dfs(i+1)
+                    else:
+                        break
+            return memo[idx]
+            # return False
+
+        return dfs(0)
+                
+
+
 
