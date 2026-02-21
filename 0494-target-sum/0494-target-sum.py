318b6973adcb985dc94ac005ff35310525b9357e
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-21 17:10:36 -0600

    Time: 448 ms (5.17%), Space: 19.7 MB (49.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,18 +2,43 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         if not(-sum(nums)<=target<=sum(nums)):
             return 0
-
         n = len(nums)
-        memo = {}
 
-        def dp(i, x):
-            if x==0 and i>=n:
-                return 1
-            if i >= n:
-                return 0
+        offset = sum(nums)
 
-            if (i,x) not in memo:
-                memo[(i,x)] = dp(i+1, x-nums[i]) + dp(i+1, x+nums[i])
-            return memo[(i,x)]
+        dp = [[0 for  _ in range(2*sum(nums)+1)] for  __ in range(n+1)]
+        dp[n][0+offset] = 1
 
-        return dp(0,target)
+        for i in range(n-1, -1, -1):
+            for x in range(2*sum(nums)+1):
+                if x - nums[i] >= 0:
+                    dp[i][x] += dp[i+1][x - nums[i]]
+                if x + nums[i] <= 2*offset:
+                    dp[i][x] += dp[i+1][x + nums[i]]
+        # a = list(range(2*sum(nums)+1))
+        # print(a)
+        # for row in dp:
+        #     print(row)
+        
+        return dp[0][target+offset]
+                
+
+
+
+
+  
+
+
+        # memo = {}
+
+        # def dp(i, x):
+        #     if x==0 and i>=n:
+        #         return 1
+        #     if i >= n:
+        #         return 0
+
+        #     if (i,x) not in memo:
+        #         memo[(i,x)] = dp(i+1, x-nums[i]) + dp(i+1, x+nums[i])
+        #     return memo[(i,x)]
+
+        # return dp(0,target)
