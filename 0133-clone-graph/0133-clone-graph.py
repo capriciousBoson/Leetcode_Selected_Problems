771986ca26889a808097b38f4e48fa12eccc2ccb
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-22 18:27:24 -0600

    Time: 55 ms (18.06%), Space: 19.9 MB (47.41%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,28 +5,29 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+from collections import deque
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node: return 
+        if not node: return
+
         node_map = {}
+        q = deque()
+        q.append(node)
 
-        Q = collections.deque()
-        Q.append(node)
+        while q:
+            curr = q.popleft()
+            node_map[curr] = Node(curr.val)
+            for ngh in curr.neighbors:
+                if ngh not in node_map:
+                    q.append(ngh)
+        # print(f"node: map  :\n{node_map}")
+        for old, new in node_map.items():
+            for ngh in old.neighbors:
+                new.neighbors.append(node_map[ngh])
 
-        while Q:
-            current = Q.popleft()
-            node_map[current] = Node(current.val)
-
-            for ngh in current.neighbors:
-                if  ngh not in node_map:
-                    Q.append(ngh)
-        # print(node_map)
-        # return
-        for  old_node, new_node in node_map.items():
-            for ngh in old_node.neighbors:
-                new_node.neighbors.append(node_map[ngh])
         return node_map[node]
+
+
 
         
