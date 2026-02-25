63a7d58f4541f13284cee87bfd58f887dabf7dec
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-24 19:22:54 -0600

    Time: 341 ms (61.64%), Space: 21.2 MB (61.24%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,27 +1,24 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        visited = [False for _ in range(n+1)]
+        time_taken = [float('inf') for _ in range(n+1)]
 
         adj = collections.defaultdict(list)
         for u,v,w in times:
             adj[u].append([v, w])
         # reach = 
 
-        res = -float('inf')
         heap = []
         heapq.heappush(heap,[0,k])
+        time_taken[k] = 0
         
         while heap:
             time, node = heapq.heappop(heap)
-            # print(f"current node : {node} | time : {time}")
             
-            
-
-            if not visited[node]:
-                visited[node] = True
-                res = max(res, time)
-                for ngh, t in adj[node]:
+            for ngh, t in adj[node]:
+                if time + t < time_taken[ngh]:
                     heapq.heappush(heap, [time + t, ngh])
-        # print(f"adj : {adj}")
-        # print(f"visited array : {visited}")
-        return res if False not in visited[1:] else -1
+                    time_taken[ngh] = time + t
+
+        res = max(time_taken[1:])
+
+        return res if res!=float('inf') else -1
