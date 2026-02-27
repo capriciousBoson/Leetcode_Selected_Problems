11b7338d6ba71270032c14facf4b968f10054a4c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-02-26 19:21:25 -0600

    Time: 3304 ms (10.76%), Space: 20.1 MB (54.43%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) ==1:
-            return 0
-        memo = {}
-        def dfs(idx):
-            if idx >= len(nums)-1:
-                return 0
-            if idx not in memo:
-                res = float('inf')
-                for x in range(1, nums[idx]+1):
-                    res = min(res, 1+ dfs(idx + x))
-                memo[(idx)] = res
-            return memo[(idx)]
-        return dfs(0)
+        reached = [float('inf') for _ in nums]
+        reached[0]=0
+        for i in range(len(nums)):
+            for x in range(1, nums[i]+1):
+                if i+x < len(nums):
+                    reached[i+x] =   min(reached[i+x], 1+reached[i])
+        return reached[-1]
 
         
