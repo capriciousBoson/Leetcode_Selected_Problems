375008f273dd1f248696d7fecf55ac1b58d0cb3c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-03 17:11:39 -0600

    Time: 4 ms (29.35%), Space: 19.3 MB (86.51%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -4,14 +4,11 @@ class Solution:
         next_greater = {}
         stk = [-1]
         for i in range(len(nums2)-1, -1, -1):
-            if stk[-1] > nums2[i]:
-                next_greater[nums2[i]] = stk[-1]
-                stk.append(nums2[i])
-            else:
-                while stk[-1] != -1 and stk[-1] <= nums2[i]:
-                    stk.pop()
-                next_greater[nums2[i]] = stk[-1]
-                stk.append(nums2[i])
+            while stk[-1] != -1 and stk[-1] <= nums2[i]:
+                stk.pop()
+                
+            next_greater[nums2[i]] = stk[-1]
+            stk.append(nums2[i])
 
         return [next_greater[n] for n in nums1]
 
