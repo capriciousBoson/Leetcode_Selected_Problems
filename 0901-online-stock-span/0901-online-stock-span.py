c7842464ea70f45abe3c00c4f821dc36f432a675
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-03 17:21:41 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,25 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.price_history = []
+        self.monotonic_stck = [-1]
+
+    def next(self, price: int) -> int:
+        self.price_history.append(price)
+
+        while self.monotonic_stck[-1]!= -1 and price> self.price_history[self.monotonic_stck[-1]]:
+            self.monotonic_stck.pop()
+
+        last_idx = self.monotonic_stck[-1]
+        curr_idx = len(self.price_history)-1
+
+        self.monotonic_stck.append(curr_idx)
+        return curr_idx - last_idx
+
+
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
