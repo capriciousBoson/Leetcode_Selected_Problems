732723c0a67d61a0f206702b67d129654d820743
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-04 20:01:05 -0600

    Time: 39 ms (13.88%), Space: 21 MB (85.48%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,22 +1,25 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         nums2 = nums + nums
-        next_greater = {}
+        # next_greater = {}
         stk = ['/']
+        res = [-1 for n in nums]
 
         for i in range(len(nums2)-1, -1, -1):
             while stk[-1] != '/' and stk[-1] <= nums2[i]:
                 stk.pop()
-            next_greater[(i, nums2[i])] = stk[-1]
+            # next_greater[(i, nums2[i])] = stk[-1]
+            if i < len(nums):
+                res[i] = stk[-1] if stk[-1] != '/' else -1
             stk.append(nums2[i])
 
-        res = []
-        for i, n in enumerate(nums):
-            ng = next_greater[(i,n)]
-            if ng != '/':
-                res.append(ng)
-            else:
-                res.append(-1)
+        # res = []
+        # for i, n in enumerate(nums):
+        #     ng = next_greater[(i,n)]
+        #     if ng != '/':
+        #         res.append(ng)
+        #     else:
+        #         res.append(-1)
 
         return res
         
