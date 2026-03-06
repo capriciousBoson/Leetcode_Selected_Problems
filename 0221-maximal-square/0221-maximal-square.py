52e6a36368d8e001cf494e6be39f180b74b20554
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-06 13:52:51 -0600

    Time: 635 ms (5.01%), Space: 124.1 MB (5.35%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,46 +1,33 @@
 class Solution:
-    def largestSquareArea(self, heights):
-        stk = [-1]
-        maximum_area = 0
-        for idx in range(len(heights)):
-            while stk[-1]!=-1 and heights[stk[-1]] >= heights[idx]:
-                p = stk.pop()
-                pse = stk[-1]
-                nse = idx
-                side = min((nse-pse-1), heights[p] )
-                maximum_area = max(maximum_area, side*side)
-            stk.append(idx)
-
-        while stk:
-            i2 = stk.pop()
-            if i2==-1: break
-            pse = stk[-1]
-            nse = len(heights)
-            side = min((nse-pse-1), heights[i2] )
-            maximum_area = max(maximum_area, side*side)
-
-        return maximum_area
 
 
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        current_heights = [0 for _ in range(cols)]
+        dirs = [(0,1), (1,0), (1,1)]
 
         max_area = 0
+        memo = {}
 
-        for i in range(rows):
-            # update current heights
-            for j in range(cols):
-                if matrix[i][j] == "1":
-                    current_heights[j] += 1
+        def dfs(r,c):
+            nonlocal max_area
+            if r >= rows or c >= cols:
+                return 0
+            if (r,c) not in memo:
+                side = float('inf')
+                
+                for dx, dy in dirs:
+                    x,y = r+dx, c+dy
+                    side = min(side, dfs(x,y))
+
+                if matrix[r][c] == '1':
+                    side += 1
                 else:
-                    current_heights[j] = 0
-  
-            # use monotonic stk to find nest smaller element 
-            #  and previous smaller element and find area
+                    side = 0
+                max_area = max(max_area, side*side)
+                memo[(r,c)] = side
+            # print(f"max side at i,j : {r,c} = side : {side}")
+            return memo[(r,c)]
 
-            max_area = max(max_area, self.largestSquareArea(current_heights))
-
-            
+        dfs(0,0)
         return max_area
         
