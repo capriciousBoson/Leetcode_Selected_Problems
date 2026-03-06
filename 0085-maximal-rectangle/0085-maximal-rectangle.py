b414e51a3be14812581a8c62c993ea31759b29a2
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-06 11:41:42 -0600

    Time: 34 ms (47.48%), Space: 24.7 MB (45.5%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,4 +1,29 @@
 class Solution:
+    def largestRectangleArea(self, heights):
+        stk = [-1]
+        maximum_area = 0
+        for idx in range(len(heights)):
+            while stk[-1]!=-1 and heights[stk[-1]] >= heights[idx]:
+                p = stk.pop()
+                pse = stk[-1]
+                nse = idx
+                # print(f"for idx :{p} pse : {pse}, nse : {nse}")
+                area = (nse-pse-1)*heights[p]
+                # print(f"calculated area : {area}")
+                maximum_area = max(maximum_area, area)
+            stk.append(idx)
+
+        while stk:
+            i2 = stk.pop()
+            if i2==-1: break
+            pse = stk[-1]
+            nse = len(heights)
+            area = (nse-pse-1)*heights[i2]
+            # print(f"for idx : {i2}, pse : {pse} and nse : {nse} , area : {area}")
+            maximum_area = max(maximum_area, area)
+        return maximum_area
+
+
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
         current_heights = [0 for _ in range(cols)]
@@ -20,28 +45,9 @@ class Solution:
             # print(f"max_area : {max_area}")
             # use monotonic stk to find nest smaller element 
             #  and previous smaller element and find area
-            # print(f"at i :{i} | current_heights : {current_heights}")
+            max_area = max(max_area, self.largestRectangleArea(current_heights))
 
-            stk = [-1]
-            for idx in range(cols):
-                while stk[-1]!=-1 and current_heights[stk[-1]] >= current_heights[idx]:
-                    p = stk.pop()
-                    pse = stk[-1]
-                    nse = idx
-                    # print(f"for idx :{p} pse : {pse}, nse : {nse}")
-                    area = (nse-pse-1)*current_heights[p]
-                    # print(f"calculated area : {area}")
-                    max_area = max(max_area, area)
-                stk.append(idx)
-
-            while stk:
-                i2 = stk.pop()
-                if i2==-1: break
-                pse = stk[-1]
-                nse = cols
-                area = (nse-pse-1)*current_heights[i2]
-                # print(f"for idx : {i2}, pse : {pse} and nse : {nse} , area : {area}")
-                max_area = max(max_area, area)
+            
 
         return max_area
             
