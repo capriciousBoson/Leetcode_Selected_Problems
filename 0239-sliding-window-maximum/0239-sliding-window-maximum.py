604127da07a6b8f2a0d6faa84ecb6b772ea26bf8
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-07 15:15:31 -0600

    Time: 169 ms (87.14%), Space: 34.7 MB (90.14%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,28 +1,34 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n= len(nums)
+        n = len(nums)
 
         res = []
-        # prev_max = -float('inf')
-        # prev_max_idx = -1
-
-        left = 0
-        right = k-1
-        heap = []
-        for i in range(k-1):
-            heapq.heappush(heap, [-nums[i], i])
-
-
-        while right < n:
-            heapq.heappush(heap, [-nums[right], right])
-            while heap[0][1] < left:
-                heapq.heappop(heap)
-
-            res.append(-heap[0][0])
-
-            right += 1
-            left += 1
+        # we implement a monotonic queue
+        q = collections.deque()
+        for i in range(k):
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+            q.append(i)
+        
+        res.append(nums[q[0]])
             
+
+        left = 1
+        right = k
+
+        while right < len(nums):
+            while q and nums[q[-1]] < nums[right]:
+                q.pop()
+            q.append(right)
+
+            if q[0] < left:
+                q.popleft()
+            
+            res.append(nums[q[0]])
+
+            left += 1
+            right += 1
+        
         return res
 
 
