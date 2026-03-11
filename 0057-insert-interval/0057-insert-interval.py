7f3fe7deba074be2c478b6e86122ec9f8e4dfd73
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-11 13:44:43 -0500

    Time: 4 ms (13.02%), Space: 21.5 MB (25.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,33 +4,42 @@ class Solution:
         if n == 0: 
             return [newInterval]
 
-        idx  = bisect.bisect_left(intervals, newInterval)
+        res = []
+        inserted = False
 
-        merge_idx = -1
+        i = 0
+        while i < n:
+            # clean insert
+            if (
+                not inserted
+                and newInterval[0] < intervals[i][0]
+                and newInterval[1] < intervals[i][0] ):
 
+                res.append(newInterval)
+                res.append(intervals[i])
+                inserted = True
+                i += 1
+                continue
+            
+            # insert and merge
+            elif (
+                not inserted
+                and ( intervals[i][0] <= newInterval[0] <= intervals[i][1]
+                or newInterval[0] <= intervals[i][0] <= newInterval[1])) : 
 
-        if idx-1 >= 0 and intervals[idx-1][0] <= newInterval[0] <= intervals[idx-1][1]:
-            # print(f"adding to left")
-            intervals[idx-1][1] = max(intervals[idx-1][1], newInterval[1])
-            merge_idx = idx-1
-
-        elif idx < n and  newInterval[1] >= intervals[idx][0] >= newInterval[0] : 
-            # print(f"adding to right")
-            intervals[idx][0] = newInterval[0]
-            intervals[idx][1] = max(intervals[idx][1], newInterval[1])
-
-            merge_idx = idx
+                start = min(newInterval[0], intervals[i][0])
+                end = newInterval[1]
         
-        else:
-            bisect.insort(intervals, newInterval)
-    
-        if merge_idx != -1:
-            i = merge_idx
-            while i+1 < len(intervals) and intervals[i][1] >= intervals[i+1][0]:
-                intervals[i][1] = max(intervals[i][1], intervals[i+1][1])
-                intervals.pop(i+1)
-                
+                while i < n and end >= intervals[i][0]:
+                    end = max(end, intervals[i][1])
+                    i += 1
+                res.append([start, end])
+                inserted = True
+                continue
+            else:
+                res.append(intervals[i])
+                i += 1
 
-        return intervals
+        if not inserted: res.append(newInterval)
+        return res
 
-        
