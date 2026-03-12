f1c16a7b722684c407d99f85f65e6d55ce3bd746
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-11 19:59:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -1,28 +1,27 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals.sort(key=lambda x: x[0])
-        n = len(queries)
-        res = [-1 for x in range(n)]
-        sorted_queries = sorted([[q, index] for index,q in enumerate(queries)])
-
-
-        heap = []
-        idx = 0
-        for q,i in sorted_queries:
-    
-            # print(f"\nq, i : {q, i}---")
-            
-            while idx< len(intervals) and q >= intervals[idx][0]:
-                s, e = intervals[idx]
-                heapq.heappush(heap, [e-s+1, e])
-                idx += 1
-            while heap  and heap[0][1] < q:
-                heapq.heappop(heap)
-            if heap: res[i] = heap[0][0]
-        return res
-
-
-                
-
-
         
+        
+        queries2 = [[i,q] for i,q in enumerate(queries)]
+        queries2.sort(key = lambda x: x[0])
+
+        intervals.sort(key = lambda x: x[0])
+
+        res = [-1 for _ in queries]
+        idx = 0
+
+        for i,q in queries2:
+            minHeap  = []
+
+            while idx < len(intervals) and q >= intervals[idx][0]:
+                s,e = intervals[idx]
+                heapq.heappush(minHeap, [e-s+1,e])
+                idx += 1
+
+            while minHeap and minHeap[0][1] < q:
+                heapq.heappop(minHeap)
+            
+            if minHeap:
+                res[i] = minHeap[0][0]
+
+        return res
