a32c5c65be2bcc826d9090990a63bdb11f69b2d4
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-12 19:11:24 -0500

    Time: 8307 ms (5.21%), Space: 125.8 MB (5.21%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        # rides.sort(key = lambda x : x[1])
+
+        start_times = collections.defaultdict(list)
+        for idx, [s,e,t] in enumerate(rides):
+            start_times[s].append(idx)
+
+       
+        idx = 0 # for rides
+        res = 0
+        memo = {}
+
+        def dfs(i):
+            if i > n:
+                return 0 
+            
+            if i not in start_times:
+                return dfs(i+1)
+
+            if i not in memo : 
+                max_profit = 0
+                for idx in start_times[i]:
+                    s,e,t = rides[idx]
+                    profit = e-s+t 
+                    while e < n+2 and e not in start_times:
+                        e += 1
+                    profit += dfs(e)
+                    max_profit = max(max_profit, profit)
+
+                max_profit = max(max_profit, dfs(i+1))
+                memo[i] = max_profit
+            return memo[i]
+        return dfs(1)
+
+
