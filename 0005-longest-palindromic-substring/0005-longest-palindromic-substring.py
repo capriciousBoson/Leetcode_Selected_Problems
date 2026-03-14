ecbd2598ca6b384bf916d4d8121d993f64475441
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-13 22:02:19 -0500

    Time: 271 ms (58.14%), Space: 19.4 MB (66.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,23 +4,20 @@ class Solution:
         max_str = ""
         n = len(s)
 
-        def dfs(l,r):
-            nonlocal max_length, max_str
-             
-            if l <0 or r >= n:
-                return 
-            if s[l]!=s[r]:
-                return
-
-            if s[l]==s[r]:
-                length = r-l + 1
-                if  length > max_length:
-                    max_length = length
-                    max_str  = s[l:r+1]
-                
-                dfs(l-1, r+1)
-
+        
         for i in range(n):
-            dfs(i,i)
-            dfs(i,i+1)
+            start_points = [[i,i], [i,i+1]]
+
+            for l,r in start_points :
+                while l>=0 and r<n and s[l]==s[r]:
+                    # print(f"str : {s[l:r+1]}")
+
+                    length = r-l + 1
+                    if  length > max_length:
+                        max_length = length
+                        max_str  = s[l:r+1]
+                    l -=1
+                    r +=1
+                    
+
         return max_str
