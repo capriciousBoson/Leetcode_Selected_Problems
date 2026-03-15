ebe2e897e2136686d2635674b65694c41f0dc144
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-15 15:00:28 -0500

    Time: 119 ms (69.08%), Space: 19.1 MB (97.33%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,23 +1,15 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        memo = {}
 
-        def expand(l,r):
-            # nonlocal ans
-            if l> r or l<0 or r>=len(s):
-                return 0
-            if (l,r) not in memo:
-                if s[l]==s[r]:
-                    memo[(l,r)] = 1 + expand(l-1, r+1)
-                else:
-                    memo[(l,r)] = 0
-            return memo[(l,r)]
+        res = 0
+        for i in range(len(s)):
+            start_points = ((i,i), (i,i+1))
+
+            for left, right in start_points:
+                while left >= 0 and right < len(s) and s[left] == s[right]:
+                    res += 1
+                    left -= 1
+                    right += 1
+        return res
                 
-        a = 0
-        for  i in range(len(s)):
-            a += expand(i,i)
-            a += expand(i, i+1)
-
-        return a
-
-        
+            
