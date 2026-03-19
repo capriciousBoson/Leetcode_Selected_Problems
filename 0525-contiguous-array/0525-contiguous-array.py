344fd6e5d485e56cdffa0d7e4a65bb4d432ed468
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-18 19:16:56 -0500

    Time: 114 ms (7.87%), Space: 25.2 MB (89.41%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -2,15 +2,12 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         diffs = {}
 
-        # zeros = [0 for _ in nums] 
-        ones = [0 for _ in nums]
-        c1,c0 = 0,0
-
+        c1  = 0
         res = 0
+
         for i in range(len(nums)):
             if nums[i]:
                 c1 += 1
-                ones[i] = c1
 
             c0 = i-c1+1
             
@@ -20,8 +17,9 @@ class Solution:
             if c1-c0 == 0:
                 res = max(res, i+1)
                 
-            pi = diffs[c1-c0]   #prefix index to remove
-            res = max(res, i-pi)
+            else:
+                pi = diffs[c1-c0]   #prefix index to remove
+                res = max(res, i-pi)
 
         return res
 
