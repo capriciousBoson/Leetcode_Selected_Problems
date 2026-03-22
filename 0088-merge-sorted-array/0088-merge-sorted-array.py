854ea19b6c920b432ec95fd15741fc9258e0029e
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-22 14:32:25 -0500

    Time: 0 ms (100%), Space: 19.4 MB (31.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,33 +5,28 @@ class Solution:
         """
 
         i1, i2 = m-1,n-1
+        end = m+n -1
 
-        if not n: return
-
-        end = m+n - 1
-
-        while i2 >=0 :
-            
-            if i1 < 0:
+        while i2>=0:
+            if i1<0:
                 nums1[end] = nums2[i2]
                 i2 -= 1
                 end -= 1
                 continue
-
-
-            if nums2[i2] >= nums1[i1]:
+            
+            if nums1[i1] > nums2[i2]:
+                nums1[end] = nums1[i1]
+                # nums1[i1] = 0
+                i1 -= 1
+                end -= 1
+            else:
                 nums1[end] = nums2[i2]
                 i2 -= 1
                 end -= 1
 
-            else:
-                nums1[end] = nums1[i1]
-                nums1[i1] = 0
- 
-                i1 -= 1
-                end -= 1
 
 
+        
 
 
 
