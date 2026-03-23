852dd93bd5db907e624ecb593982c4a7b96f8b1a
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-23 15:43:04 -0500

    Time: 387 ms (5.04%), Space: 220 MB (5.46%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,25 +2,31 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
 
         removed = 0
+        memo = {}
 
         def dfs(l,r):
+
             nonlocal removed
             if l > r:
                 return True
-            if s[l]==s[r]:
-                return dfs(l+1, r-1)
-            
-            else:
-                if removed >= 1:
-                    return False
+
+            if (l,r) not in memo:
+                    
+                if s[l]==s[r]:
+                    memo[(l,r)] =  dfs(l+1, r-1)
+                
                 else:
-                    removed += 1
-                    ans = False
-                    if s[l+1] == s[r]:
-                        ans = ans or dfs(l+1, r)
-                    if s[l] == s[r-1]:
-                        ans = ans or dfs(l, r-1)
-                    return ans 
+                    if removed >= 1:
+                        return False
+                    else:
+                        removed += 1
+                        ans = False
+                        if s[l+1] == s[r]:
+                            ans = ans or dfs(l+1, r)
+                        if s[l] == s[r-1]:
+                            ans = ans or dfs(l, r-1)
+                        memo[(l,r)] = ans 
+            return memo[(l,r)]
                     
         return dfs(0, len(s)-1)
 
