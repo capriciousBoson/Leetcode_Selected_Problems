f28864668dd1da8050851d17c0db3ee40bec9c24
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-24 17:39:14 -0500

    Time: 345 ms (44.46%), Space: 21.2 MB (87.26%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,24 +1,30 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        time_taken = [float('inf') for _ in range(n+1)]
 
-        adj = collections.defaultdict(list)
+        adj = {}
         for u,v,w in times:
-            adj[u].append([v, w])
-        # reach = 
+            if u not in adj:
+                adj[u] = []
+            adj[u].append([v,w])
 
-        heap = []
-        heapq.heappush(heap,[0,k])
-        time_taken[k] = 0
+        min_time = [float('inf') for _ in range(n+1)]
+        min_time[k] = 0
+        q = collections.deque([[k, 0]])
+
+        while q:
+            node,t = q.popleft()
+
+            if node not in adj:
+                continue
+            for ngh,nt in adj[node]:
+                if t+nt < min_time[ngh]:
+                    min_time[ngh] = t+nt
+                    q.append([ngh, min_time[ngh]])
         
-        while heap:
-            time, node = heapq.heappop(heap)
-            
-            for ngh, t in adj[node]:
-                if time + t < time_taken[ngh]:
-                    heapq.heappush(heap, [time + t, ngh])
-                    time_taken[ngh] = time + t
+        max_time = max(min_time[1:])
+        print(f"max_time : {max_time} | \nmin_times : {min_time}")
+        if max_time == float('inf'):
+            return -1
+        return max_time
 
-        res = max(time_taken[1:])
-
-        return res if res!=float('inf') else -1
+        
