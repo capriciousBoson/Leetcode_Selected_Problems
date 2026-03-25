9196cc60f3eac4d872435f790f43d8fd723a5b7c
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-25 18:32:40 -0500

    Time: 3 ms (44.43%), Space: 19.5 MB (13.64%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,47 @@
+import math
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) <2:
+            return len(chars)
+
+        i = 1
+        current_count = 1
+        left = 0
+
+        while i < len(chars):
+            if chars[i]==chars[i-1]:
+                current_count += 1
+                
+            else:
+                left += 1
+                if current_count >1:
+                    
+                    for c in str(current_count):
+                        chars[left] = c
+                        left += 1
+                    
+                chars[left] = chars[i]
+
+                current_count = 1
+
+            i += 1
+
+        print(f"left : {left} | current_count = {current_count}")
+        print(f"chars[:left] : {chars[:left]}")
+        print(f"chars : {chars}")
+
+        left += 1
+        if current_count>1:
+            
+            for c in str(current_count):
+                chars[left] = c
+                left += 1
+
+        return left
+
+
+            
+
+
+        
+        
