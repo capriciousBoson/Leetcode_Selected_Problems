1aa27811d1806c5800ca12414e4e92b3cdf600d5
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-03-24 19:32:19 -0500

    Time: 781 ms (61.75%), Space: 90.6 MB (57.43%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,81 +1,29 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-
-        n = len(points)
-        def weight(i,j):
-            return abs(points[i][0] - points[j][0]) + abs(points[i][1]-points[j][1])
-
-        dist = {(x,y):float('inf') for [x,y] in points}
-        h = []
-
-        
-        
-        heapq.heappush(h, (0, 0))
         visited = [False for _ in points]
-
-        print(f"h : {h}")
+        
+        n = len(points)
         res = 0
+        heap = [[0, 0]]
         edges = 0
-        while h and edges < n:
-            d,node = heapq.heappop(h)
-            if visited[node]: continue
+
+        while heap and edges < n:
+            wt,node = heapq.heappop(heap)
+
+            if visited[node] : continue
             visited[node] = True
-            res += d
+
+            res += wt
             edges += 1
-            for idx in range(n):
-                if visited[idx] : continue
-                d_ = weight(node, idx)
-                heapq.heappush(h, (d_, idx))
+
+            for ngh in range(n):
+                if visited[ngh] : continue
+
+                x,y = points[node]
+                i,j = points[ngh]
+                new_wt = abs(x-i) + abs(y-j)
+
+                heapq.heappush(heap, [new_wt,ngh])
 
         return res
-
-
-
-
-        # print(F"dist : {dist}")            
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # cost = [[float('inf') for _ in range(n)] for __ in range(n)]
-
-        # for i in range(n):
-        #     for j in range(n):
-        #         dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
-        #         cost[i][j] = dist
-
-        # res = 0
-        # for k in range(n):
-        #     for r in range(n):
-        #         if k==r: continue
-        #         for c in range(n):
-        #             if c==k: continue
-        #             cost[r][c] = min(cost[r][c], cost[r][k]+cost[k][r]) 
-        #             res += cost[r][c]
-
-        # for row in cost:
-        #     print(row)
-
-        # return res//2
-                
+        
