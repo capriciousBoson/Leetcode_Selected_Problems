42d31715ee74ceea424cb92b795fa8d16ead5fa8
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-04-02 15:42:10 -0500

    Time: 181 ms (6.14%), Space: 33.4 MB (5.04%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,11 +1,16 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
 
+        eid = {}
+    
+
         adj = collections.defaultdict(list)
 
         for i,[u,v] in enumerate(edges):
-            adj[u].append([v,i])
-            adj[v].append([u,i])
+            adj[u].append(v)
+            adj[v].append(u)
+            eid[(u,v)] = i
+            eid[(v,u)] = i
 
         heap = []
         heapq.heappush(heap, [-1, start_node])
@@ -21,9 +26,11 @@ class Solution:
             if node == end_node:
                 return -p
 
-            for ngh, eid in adj[node]:
+            for ngh in adj[node]:
+                print(f"ngh - {ngh}")
                 if not visited[ngh]:
-                    heapq.heappush(heap, [p*succProb[eid], ngh])
+
+                    heapq.heappush(heap, [p*succProb[eid[(ngh, node)]], ngh])
 
     
         return 0
