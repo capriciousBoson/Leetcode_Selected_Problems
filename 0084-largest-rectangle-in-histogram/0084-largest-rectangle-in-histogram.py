7ed4dfd929ab46411cf19ce44428d08f455e27dd
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-04-11 17:53:26 -0500

    Time: 284 ms (5.02%), Space: 32.9 MB (31.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,51 +1,33 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        res = 0
-        n = len(heights) 
+        n = len(heights)
+        next_smaller = [-1 for _ in range(n)]
+        prev_smaller = [-1 for _ in range(n)]
 
         stk = [-1]
-        for i in range(n):
-            while stk[-1]!=-1 and heights[stk[-1]] >= heights[i]:
-                idx = stk.pop()
-                ps = stk[-1] 
-                ns = i
-                area = heights[idx]*(ns-ps-1)
-                res = max(res, area)
+        for i, h in enumerate(heights):
+            while stk[-1]!=-1 and heights[stk[-1]] >= h:
+                stk.pop()
+            prev_smaller[i] = stk[-1]
             stk.append(i)
-        
-        while stk:
-            idx = stk.pop()
-            if idx == -1: break
-            ns = n
-            ps = stk[-1]
-            area = heights[idx]*(ns-ps-1)
+        print(prev_smaller)
+
+        stk = [n]
+        for i in range(n-1, -1, -1):
+            while stk[-1]!=n and heights[stk[-1]] >= heights[i]:
+                stk.pop()
+            next_smaller[i] = stk[-1]
+            stk.append(i)
+
+        print(next_smaller)
+
+        res = heights[0]
+        for i in range(n):
+            width = next_smaller[i]-1 - prev_smaller[i]
+            area = width*heights[i]
             res = max(res, area)
         return res
-
-                
-
-        # ns = [-1 for n in heights]
-        # # ps = [-1 for n in heights]
-        # res = 0
-
-        # n = len(heights)
-        # stk = [n]
-        # for i in range(n-1, -1, -1):
-        #     while stk[-1]!=n and heights[stk[-1]] >= heights[i]:
-        #         stk.pop()
-        #     ns[i] = stk[-1]
-        #     stk.append(i)
         
-        # stk = [-1]
-        # for i in range(n):
-        #     while stk[-1]!=-1 and heights[stk[-1]] >= heights[i]:
-        #         stk.pop()
-        #     ps_i = stk[-1]
-        #     stk.append(i)
 
-        #     # calculate area for current height
-        #     area = heights[i]*(ns[i]-ps_i-1)
-        #     res = max(res, area)
 
-        # return res
         
