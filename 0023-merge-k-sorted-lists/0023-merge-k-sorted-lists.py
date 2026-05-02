f8cff99d64cdab1c1344b0136c576dc70f1472f2
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-05-01 23:04:29 -0500

    Time: 75 ms (12.65%), Space: 22.5 MB (93.25%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,34 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        # if not len(lists) :
-        #     return 
+        temp = ListNode()
+        curr = temp
+        while True:
 
-        all_nodes = []
-        counter = 0
-        for head in lists:
-            curr = head
-            while curr:
-                # print(f"curr.val, curr = {curr.val, curr}")
-                heapq.heappush(all_nodes, (curr.val,counter, curr))
-                curr = curr.next
-                counter +=1
+            min_head = float('inf')
+            for l in lists:
+                if not l:
+                    continue
+                if l.val < min_head:
+                    min_head = l.val
+            
+            if min_head == float('inf'):
+                break
+            for i in range(len(lists)):
+                node = lists[i]
+                if node !=None and node.val==min_head:
+                    lists[i] = node.next
+                    node.next = None
+                    curr.next = node
+                    curr = curr.next
 
-        if not len(all_nodes): return
-        # print(heapq.heappop(all_nodes)[2])
-        res = heapq.heappop(all_nodes)[2]
-        curr = res
-        while all_nodes:
-            curr.next = heapq.heappop(all_nodes)[2]
-            curr = curr.next
-        return res
-                
-        
+        return temp.next
+     
 
-
-        
