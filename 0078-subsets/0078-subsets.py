98af2ea565afd32cfa80008fe1abe256d02a14e6
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-07-22 17:19:39 -0500

    Time: 0 ms (100%), Space: 19.4 MB (66.86%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(list)
+        n = len(nums)
         res = []
-
-        def dfs(i,subset):
-            if i==n:
-                res.append(subset)
-                return
-            
-            dfs(i + 1, subset + [nums[i]])
-            dfs(i+1, subset)
-            return 
-        
-        dfs(0,[])
+        for i in range(2**n):
+            bits = f'{i:0{n}b}'
+            subset = [nums[j] for j in range(n) if bits[j] == '1']
+            res.append(subset)
         return res
+
+        # def dfs(i,subset):
+        #     if i==n:
+        #         res.append(subset)
+        #         return
+            
+        #     dfs(i + 1, subset + [nums[i]])
+        #     dfs(i+1, subset)
+        #     return 
+        
+        # dfs(0,[])
+        # return res
         
