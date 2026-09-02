f0d9dabc1fd27a603f4998445efe0635d9791e71
ASHISH   SHAH <54185524+capriciousBoson@users.noreply.github.com>
2026-09-02 14:04:13 -0500

    Time: 20 ms (12.17%), Space: 23.9 MB (26.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,14 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
-
-        for word in strs:
-            sig = [0 for _ in range(26)]
-            for c in word:
-                sig[ord(c)-ord('a')] += 1
-            groups[tuple(sig)].append(word)
-
-        return list(groups.values())
         
+        groups = collections.defaultdict(list)
+
+        for  s in strs:
+            key = [0 for _ in range(26)]
+            for char in s:
+                key[ord(char)-97] += 1
+            
+            key = tuple(key)
+            groups[key].append(s)
+        
+        return list(groups.values())
